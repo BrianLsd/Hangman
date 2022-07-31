2cf07cb41c8691b9459b406d958846318ba00c8b
Zhe Li <zhe.li99@ed.cna.nl.ca>
2022-07-31 00:04:05 -0230

    Main method created

---
+++ src/GameLauncher.java
@@ -0,0 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class GameLauncher {
+    public static void main(String[] args) {
+        boolean isValid = false;
+
+        System.out.println("---------------------------------------------------------------");
+        System.out.println("Welcome to Hangman!");
+
+        while (!isValid){
+            Scanner input = new Scanner(System.in);
+            System.out.println("Please choose a difficulty level: 1-Easy 2-Medium 3-Hard");
+            try{
+                int choice = input.nextInt();
+                if (choice <= 3 && choice >= 1){
+                    isValid = true;
+                } else {
+                    System.out.println("Invalid choice, please enter again");
+                }
+            }catch (InputMismatchException e){
+                System.out.println("Invalid choice, please enter an integer");
+            }catch (Exception ex){
+                System.out.println("Unexpected error, closing program");
+                ex.printStackTrace();
+                System.exit(1);
+            }
+        }
+    }
+}
